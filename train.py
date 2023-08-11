d04e950abe7f0d8470bd32579489e3438eabc638
yousif <ynymansour@gmail.com>
2023-08-10 20:38:31 -0400

    make dataset and training work

--- train.py
+++ train.py
@@ -2,10 +2,10 @@ import torch
 import os
 from huggingface_hub import login
 import json
-from datasets import load_dataset
-from peft import LoraConfig
+from datasets import Dataset
+from peft import LoraConfig, AutoPeftModelForCausalLM
 from tqdm import tqdm
-from transformers import AutoModelForCausalLM, BitsAndBytesConfig, TrainingArguments, AutoTokenizer, AutoPeftModelForCausalLM
+from transformers import AutoModelForCausalLM, BitsAndBytesConfig, TrainingArguments, AutoTokenizer
 import datetime
 from trl import SFTTrainer
 tqdm.pandas()
@@ -20,8 +20,8 @@ with open("config.json", "r") as f:
 
 # Step 1: Load arguments
 print("Step 1: Load arguments")
-load_in_8bit = get_config_value(config, "load_in_8bit", True)
-load_in_4bit = get_config_value(config, "load_in_4bit", False)
+load_in_4bit = get_config_value(config, "load_in_4bit", True)
+load_in_8bit = get_config_value(config, "load_in_8bit", False)
 model_name = get_config_value(config, "model_name", "gpt2")
 dataset_dir = get_config_value(config, "dataset_dir", "./data")
 output_dir = get_config_value(config, "output_dir", "./output")
@@ -42,7 +42,7 @@ hf_repo_name = get_config_value(
 
 # Step 2: Prepare model config
 print("Step 2: Prepare model config")
-device_map = "auto"
+device_map = None
 if load_in_4bit:
     quantization_config = BitsAndBytesConfig(load_in_4bit=load_in_4bit)
     torch_dtype = torch.bfloat16
@@ -65,7 +65,27 @@ model = AutoModelForCausalLM.from_pretrained(
 
 # Step 4: Load the dataset
 print("Step 4: Load the dataset")
-dataset = load_dataset("text", data_dir=dataset_dir)
+# dataset = load_dataset("text", data_dir=dataset_dir, split="train")
+# read dataset_dir
+_texts = []
+for filename in os.listdir(dataset_dir):
+    with open(os.path.join(dataset_dir, filename), "r") as f:
+        _texts.append(f.read())
+
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model_max_length = tokenizer.model_max_length
+
+# Instead of filtering, we'll break texts into chunks
+chunks = []
+for text in _texts:
+    tokenized_text = tokenizer.encode(text, add_special_tokens=True)
+    for i in range(0, len(tokenized_text), model_max_length):
+        chunk = tokenized_text[i:i+model_max_length]
+        chunks.append(tokenizer.decode(chunk))
+
+texts = chunks
+
+dataset = Dataset.from_dict({"text": texts})
 
 # Step 5: Define the training arguments
 print("Step 5: Define the training arguments")
@@ -98,32 +118,39 @@ trainer = SFTTrainer(
     train_dataset=dataset,
     dataset_text_field="text",
     peft_config=peft_config,
+    max_seq_length=model_max_length,
 )
 trainer.train()
 
 # Step 8: Save the model locally
 print("Step 8: Save the model locally")
-final_checkpoints_dir = os.path.join(output_dir, "train_checkpoints")
+final_checkpoints_dir = os.path.join(
+    output_dir, f"train_checkpoint-on-{datetime.datetime.now().strftime('%Y_%m_%d')}")
 trainer.model.save_pretrained(final_checkpoints_dir)
-
-# Free memory for merging weights
-del model
-torch.cuda.empty_cache()
-model = AutoPeftModelForCausalLM.from_pretrained(
-    final_checkpoints_dir, device_map=device_map, torch_dtype=torch.bfloat16)
 
 # merge lora weights, if using PEFT
 if use_peft:
+    # Free memory for merging weights
+    del model
+    torch.cuda.empty_cache()
+    model = AutoPeftModelForCausalLM.from_pretrained(
+        final_checkpoints_dir, device_map=device_map, torch_dtype=torch.bfloat16)
+
     model = model.merge_and_unload()
-output_merged_dir = os.path.join(output_dir, "final_merged_checkpoint")
-model.save_pretrained(output_merged_dir, safe_serialization=True)
+    output_merged_dir = os.path.join(output_dir, "final_merged_checkpoint")
+    model.save_pretrained(output_merged_dir, safe_serialization=True)
 
 # Step 9: Push the model to the Hub
 if hf_token:
     print("Step 9: Push the model to the Hub")
-    login(token="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")
+    login(token=hf_token)
     tokenizer = AutoTokenizer.from_pretrained(model_name)
     tokenizer.push_to_hub(hf_repo_name)
-model.push_to_hub(hf_repo_name)
+    model.push_to_hub(hf_repo_name)
 
 print("Done!")
+
+# Load the model locally
+# output_dir = "./output"
+# final_checkpoints_dir = os.path.join(output_dir, f"train_checkpoint-on-2023_08_10")
+# AutoModelForCausalLM.from_pretrained(final_checkpoints_dir, torch_dtype=torch.bfloat16)
